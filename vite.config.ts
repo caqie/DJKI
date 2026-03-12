b0ba438b36c5465164d770d9b2f0b19d3ac33e08
Caqie_Studio <robie.doank86@gmail.com>
2026-03-12 08:56:54 +0700

    feat: Update Vite and dependencies
    Upgrades Vite to version 5.4.10 and adjusts related dependencies. This ensures compatibility and leverages the latest features and performance improvements from Vite.

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,17 @@
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
 
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [react(), tailwindcss()],
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, './src'),
-        }
-      }
-    };
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  server: {
+    port: 3000,
+    host: '0.0.0.0',
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 });
